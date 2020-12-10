07fb21ace6bc6f55e7a4356a6bab47665374e8f3
Maximiliano <maxirpc2607@gmail.com>
2020-12-10 12:18:13 -0300

    fix ico

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <link rel="icon" href="<?=base_url()?>/assets/img/logo/evolveteamfavico512x512" type="image">
+    <link rel="icon" href="<?=base_url()?>/assets/img/logo/evolveteamfavico512x512.png" type="image/png">
     <title>Evolve | Panel</title>
 
     <!-- Font Awesome Icons -->
