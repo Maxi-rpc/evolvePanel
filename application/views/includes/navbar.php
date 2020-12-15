4e0ab4ef11e8e63376a77834b0caa657bd114441
Maximiliano <maxirpc2607@gmail.com>
2020-12-15 14:58:49 -0300

    Update navbar.php

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -151,7 +151,7 @@
           </li>
           <!-- Icono Salir  -->
           <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="<?= base_url('login/logout'); ?>">
+            <a class="nav-link" href="<?= base_url('login/logout'); ?>">
             <i class="fas fa-sign-out-alt"></i>
             </a>
           </li>
