be1ca8fbe75d008c263a4ecc9963678a07754631
Maximiliano <maxirpc2607@gmail.com>
2020-12-17 14:20:07 -0300

    Update staff.php

--- application/views/main/staff.php
+++ application/views/main/staff.php
@@ -1,5 +1,5 @@
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper bg-dark">
+<div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <div class="content-header">
           <div class="container-fluid">
