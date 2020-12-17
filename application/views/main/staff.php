18796b871bb0d0d7e2c923fb2e3c3f6848d6f378
Maximiliano <maxirpc2607@gmail.com>
2020-12-17 14:22:46 -0300

    Update staff.php

--- application/views/main/staff.php
+++ application/views/main/staff.php
@@ -5,7 +5,7 @@
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h1 class="m-0 text-white">Staff</h1>
+                <h1 class="m-0 text-black">Staff</h1>
               </div>
               <!-- /.col -->
               <div class="col-sm-6">
