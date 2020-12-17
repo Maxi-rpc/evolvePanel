761e324c6484387c48a9e36c2b3341bd126aaaa6
Maximiliano <maxirpc2607@gmail.com>
2020-12-17 16:16:11 -0300

    Update adminarea.php

--- application/views/main/adminarea.php
+++ application/views/main/adminarea.php
@@ -126,7 +126,7 @@
               <div class="col-md-6">
               <div class="card card-primary collapsed-card">
               <div class="card-header">
-                <h3 class="card-title">Expandable</h3>
+                <h3 class="card-title">Crear Usuario</h3>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
