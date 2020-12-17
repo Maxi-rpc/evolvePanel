8728679003f0f284230d430ca72e50ced662dc05
Maximiliano <maxirpc2607@gmail.com>
2020-12-17 17:07:20 -0300

    Update adminarea.php

--- application/views/main/adminarea.php
+++ application/views/main/adminarea.php
@@ -138,36 +138,31 @@
               <div class="card-body">
                 <!-- form start -->
                 <form role="form">
-                  
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                      <label>Nombre</label>
+                      <input type="text" name="firstname" value="" class="form-control" placeholder="Nombre">
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      <label>Apellido</label>
+                      <input type="text" name="lastname" value="" class="form-control" placeholder="Apellido">
                     </div>
                     <div class="form-group">
-                      <label for="exampleInputFile">File input</label>
-                      <div class="input-group">
-                        <div class="custom-file">
-                          <input type="file" class="custom-file-input" id="exampleInputFile">
-                          <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                        </div>
-                        <div class="input-group-append">
-                          <span class="input-group-text" id="">Upload</span>
-                        </div>
+                      <label>Email</label>
+                      <input type="email" name="email" value="" class="form-control" placeholder="Email">
+                    </div>
+                    <!-- select -->
+                    <div class="form-group">
+                        <label>Select</label>
+                        <select class="form-control">
+                          <option>option 1</option>
+                          <option>option 2</option>
+                          <option>option 3</option>
+                          <option>option 4</option>
+                          <option>option 5</option>
+                        </select>
                       </div>
-                    </div>
-                    <div class="form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                  
-                  
-
                   <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Crear</button>
                   </div>
                 </form>
               </div>
