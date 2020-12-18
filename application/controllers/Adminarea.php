287e52001d7844e2fdbd84760b57310cd247cf84
Maximiliano <maxirpc2607@gmail.com>
2020-12-18 01:22:04 -0300

    Update Adminarea.php

--- application/controllers/Adminarea.php
+++ application/controllers/Adminarea.php
@@ -5,7 +5,7 @@ class Adminarea extends CI_Controller {
     public function __construct()
 	{
 		parent::__construct();
-        $this->load->model('Users','Perfiles');
+        $this->load->model(['Users','Perfiles']);
         $this->load->helper('vistas');
     }
     
