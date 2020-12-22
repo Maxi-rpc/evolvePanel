5917c9b2f26bd3bd8f1685d173af6b8b57e03f68
Maximiliano <maxirpc2607@gmail.com>
2020-12-22 14:33:02 -0300

    Update leagueoflegends.php

--- application/views/main/leagueoflegends.php
+++ application/views/main/leagueoflegends.php
@@ -23,33 +23,32 @@
         <!-- /.content-header -->
         <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card collapsed-card">
-              <div class="card-header">
-                <h3 class="card-title">Buscar Partida</h3>
-                
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                    </button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card collapsed-card">
+                        <div class="card-header">
+                            <h3 class="card-title">Buscar Partida</h3>
+                            
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
                 </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                
-              </div>
-              <!-- /.card-body -->
+                <!-- /.col -->
             </div>
-            <!-- /.card -->
-          </div>
-          <!-- /.col -->
+            <!-- /.row -->
         </div>
-        <!-- /.row -->
-      </div>
-      <!-- /.container-fluid -->
+        <!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-  </div>
 </div>
 <!-- /.content-wrapper -->
