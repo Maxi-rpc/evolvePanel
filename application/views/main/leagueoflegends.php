1ea2b4609a1f4e2d2efb5a9fcf9f39a61c6d5c61
Maximiliano <maxirpc2607@gmail.com>
2020-12-22 14:27:43 -0300

    Update leagueoflegends.php

--- application/views/main/leagueoflegends.php
+++ application/views/main/leagueoflegends.php
@@ -26,9 +26,14 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-12">
-            <div class="card">
+            <div class="card collapsed-card">
               <div class="card-header">
-                <h3 class="card-title">Staff Completo</h3>
+                <h3 class="card-title">Buscar Partida</h3>
+                
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                    </button>
+                </div>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
