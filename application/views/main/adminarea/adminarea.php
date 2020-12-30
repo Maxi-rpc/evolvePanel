08e525820dd3255d17d8fabb6862bc4029e4f341
Maximiliano <maxirpc2607@gmail.com>
2020-12-30 12:11:46 -0300

    Update adminarea.php

--- application/views/main/adminarea/adminarea.php
+++ application/views/main/adminarea/adminarea.php
@@ -29,24 +29,17 @@
 
             <div class="row">
               <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box bg-info">
-                  <span class="info-box-icon"><i class="far fa-bookmark"></i></span>
+                <div class="info-box mb-3 bg-warning">
+                    <span class="info-box-icon"><i class="fas fa-tag"></i></span>
 
-                  <div class="info-box-content">
-                    <span class="info-box-text">Bookmarks</span>
-                    <span class="info-box-number">41,410</span>
-
-                    <div class="progress">
-                      <div class="progress-bar" style="width: 70%"></div>
+                    <div class="info-box-content">
+                      <span class="info-box-text">Inventory</span>
+                      <span class="info-box-number">5,200</span>
                     </div>
-                    <span class="progress-description">
-                      70% Increase in 30 Days
-                    </span>
+                    <!-- /.info-box-content -->
                   </div>
-                  <!-- /.info-box-content -->
+                  <!-- /.info-box -->
                 </div>
-                <!-- /.info-box -->
-              </div>
               <!-- /.col -->
               <div class="col-md-3 col-sm-6 col-12">
                 <div class="info-box mb-3 bg-warning">
@@ -62,44 +55,30 @@
               </div>
               <!-- /.col -->
               <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box bg-warning">
-                  <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
+                <div class="info-box mb-3 bg-warning">
+                    <span class="info-box-icon"><i class="fas fa-tag"></i></span>
 
-                  <div class="info-box-content">
-                    <span class="info-box-text">Events</span>
-                    <span class="info-box-number">41,410</span>
-
-                    <div class="progress">
-                      <div class="progress-bar" style="width: 70%"></div>
+                    <div class="info-box-content">
+                      <span class="info-box-text">Inventory</span>
+                      <span class="info-box-number">5,200</span>
                     </div>
-                    <span class="progress-description">
-                      70% Increase in 30 Days
-                    </span>
+                    <!-- /.info-box-content -->
                   </div>
-                  <!-- /.info-box-content -->
+                  <!-- /.info-box -->
                 </div>
-                <!-- /.info-box -->
-              </div>
               <!-- /.col -->
               <div class="col-md-3 col-sm-6 col-12">
-                <div class="info-box bg-danger">
-                  <span class="info-box-icon"><i class="fas fa-comments"></i></span>
+                <div class="info-box mb-3 bg-warning">
+                    <span class="info-box-icon"><i class="fas fa-tag"></i></span>
 
-                  <div class="info-box-content">
-                    <span class="info-box-text">Comments</span>
-                    <span class="info-box-number">41,410</span>
-
-                    <div class="progress">
-                      <div class="progress-bar" style="width: 70%"></div>
+                    <div class="info-box-content">
+                      <span class="info-box-text">Inventory</span>
+                      <span class="info-box-number">5,200</span>
                     </div>
-                    <span class="progress-description">
-                      70% Increase in 30 Days
-                    </span>
+                    <!-- /.info-box-content -->
                   </div>
-                  <!-- /.info-box-content -->
+                  <!-- /.info-box -->
                 </div>
-                <!-- /.info-box -->
-              </div>
               <!-- /.col -->
             </div>
             <!-- /.info Box row -->
