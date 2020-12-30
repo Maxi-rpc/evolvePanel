075fb48a6fcf00f27043d69ce5c05bdd487f58bf
Maximiliano <maxirpc2607@gmail.com>
2020-12-30 12:59:43 -0300

    Update adminarea.php

--- application/views/main/adminarea/adminarea.php
+++ application/views/main/adminarea/adminarea.php
@@ -137,6 +137,27 @@
             </div>
             <!-- /.info Box row -->
 
+            <div class="row">
+              <div class="col-12">
+                <div class="card card-primary collapsed-card">
+                  <div class="card-header">
+                    <h3 class="card-title">Listado Users</h3>
+
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+                    <!-- /.card-tools -->
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    Tabla
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+              </div>
+            </div>
+
             <!-- =========================================================== -->
           </div>
           <!-- /.container-fluid -->
