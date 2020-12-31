68096208dc69d089ebaa26eb648521148a5c2005
Maximiliano <maxirpc2607@gmail.com>
2020-12-31 12:52:22 -0300

    Update edit.php

--- application/views/main/perfil/edit.php
+++ application/views/main/perfil/edit.php
@@ -41,7 +41,7 @@
                       <div class="alert alert-danger alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                         <h5><i class="icon fas fa-ban"></i> Alert!</h5>
-                        <?php echo validation_errors('<li>', '</li>'); ?>
+                        <?php echo validation_errors(); ?>
                       </div>
                     <?php } ?>
 
