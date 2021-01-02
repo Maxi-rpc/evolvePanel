c11440dd9d2efc9cf25c175b472441c5958a2987
Maximiliano <maxirpc2607@gmail.com>
2021-01-02 18:48:35 -0300

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -10,9 +10,9 @@
                 <div class="alert alert-warning alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                     <h5><i class="icon fas fa-exclamation-triangle"></i> Alert!</h5>
-                    <?php validation_errors(); ?>
+                    <?php echo validation_errors(); ?>
                 </div>
-            <?php }; ?>
+            <?php } ?>
         <p class="login-box-msg">Iniciar Sesión</p>
 
         <form action="<?php base_url() ?>login/validate" method="post" id="">
