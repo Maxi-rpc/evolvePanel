421a47f055a42e9723859fbc24b74fb1e4caea66
Maximiliano <maxirpc2607@gmail.com>
2021-01-02 18:53:55 -0300

    Update login_rules_helper.php

--- application/helpers/auth/login_rules_helper.php
+++ application/helpers/auth/login_rules_helper.php
@@ -1,24 +1,21 @@
 <?php
-if (!function_exists('getCreateUserRules')) {
-	function getLoginRules()
-	{
-		return [
-			[
-				'field' => 'email',
-				'label' => 'Email',
-				'rules' => 'required|trim',
-				'errors' => [
-					'required' => 'El %s es requerido',
-				],
+function getLoginRules(){
+	return [
+		[
+			'field' => 'email',
+			'label' => 'Email',
+			'rules' => 'required|trim',
+			'errors' => [
+				'required' => 'El %s es requerido',
 			],
-			[
-				'field' => 'password',
-				'label' => 'Password',
-				'rules' => 'required|trim',
-				'errors' => [
-					'required' => 'El %s es requerido',
-				],
+		],
+		[
+			'field' => 'password',
+			'label' => 'Password',
+			'rules' => 'required|trim',
+			'errors' => [
+				'required' => 'El %s es requerido',
 			],
-		];
-	}
+		],
+	];
 }
