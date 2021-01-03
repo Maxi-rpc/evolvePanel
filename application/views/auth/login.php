704196fd634fc9c9345cbeb197990d6d58387744
Maximiliano <maxirpc2607@gmail.com>
2021-01-03 18:06:21 -0300

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,73 +1,90 @@
 <!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <link rel="icon" href="<?=base_url()?>assets/img/logo/evolveteamfavico512x512.png" type="image/png">
-        <title>Evolve | Panel</title>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="<?=base_url()?>assets/img/logo/evolveteamfavico512x512.png" type="image/png">
+    <title>Evolve | Panel</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous"/>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/adminlte.min.css" />
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"/>
+</head>
 
-        <!-- Font Awesome Icons -->
-        <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
-            integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc"
-            crossorigin="anonymous"
-        />
-        <!-- Theme style -->
-        <link rel="stylesheet" href="<?= base_url() ?>assets/css/adminlte.min.css" />
-        <!-- Google Font: Source Sans Pro -->
-        <link
-            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700"
-            rel="stylesheet"
-        />
-    </head>
-    <body class="hold-transition login-page">
-        <div class="login-box ">
-            <div class="login-logo"> 
-                <a href="<?= base_url(); ?>"><img src="" alt="">  EVOLVE | PANEL</a>
-            </div>
-            <!-- /.login-logo -->
-            <div class="card">
-                <div class="card-body login-card-body">
-                    <p class="login-box-msg">Iniciar Sesión</p>
-                    <form action="<?= base_url('login/validate'); ?>" method="POST" id="formLogin">
-                        <div class="input-group mb-3" id="email">
-                        <input name="email" type="email" class="form-control" placeholder="Email">
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="<?=base_url('assets/');?>index2.html"><b>Admin</b>LTE</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Ingresa tus credenciales para inicar sesión</p>
+
+                <form action="<?= base_url('login/validate'); ?>" method="POST" id="frm_login">
+                    <div class="input-group mb-3" id="email">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                                <span class="fas fa-envelope"></span>
                             </div>
                         </div>
                         <div class="invalid-feedback"></div>
-                        </div>
-                        <div class="input-group mb-3" id="password">
-                        <input name="password" type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="input-group mb-3" id="password">
+                        <input type="password" name="password" class="form-control" placeholder="Contraseña">
                         <div class="input-group-append">
                             <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
+                                <span class="fas fa-lock"></span>
                             </div>
                         </div>
                         <div class="invalid-feedback"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Recordarme
+                                </label>
+                            </div>
                         </div>
                         <!-- /.col -->
                         <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+                            <button type="submit" class="btn btn-primary btn-block">Entrar</button>
                         </div>
                         <!-- /.col -->
-                        </div>
-                    </form>
-                    <p class="mb-1">
-                        <a href="forgot-password.html">Olvidé mi contraseña</a>
-                    </p>
-                </div>
-                <!-- /.login-card-body -->
+                    </div><br>
+                    <div class="form-group" id="alert">
+
+                    </div>
+                </form>
+
+                <p class="mb-1">
+                    <a href="forgot-password.html">Olvidé mi clave</a>
+                </p>
+                <p class="mb-0">
+                    <a href="<?=base_url();?>" class="text-center">Registrarme</a>
+                </p>
             </div>
+            <!-- /.login-card-body -->
         </div>
-        <!-- /.login-box -->
-        <!-- REQUIRED SCRIPTS -->
-        <!-- jQuery -->
-        <script src="<?= base_url() ?>assets/js/jquery/jquery.js"></script>
-        <script src="<?= base_url() ?>assets/js/auth/login.js"></script>
-    </body>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="<?=base_url('assets/');?>js/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=base_url('assets/');?>js/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=base_url('assets/');?>js/adminlte.min.js"></script>
+
+    <script src="<?=base_url('assets/js/auth/login.js')?>"></script>
+
+</body>
+
 </html>
