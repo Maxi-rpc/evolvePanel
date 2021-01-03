955bda27f0b5cb6a2cc85c78a22638430d257d11
Maximiliano <maxirpc2607@gmail.com>
2021-01-03 17:57:36 -0300

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -67,7 +67,7 @@
         <!-- /.login-box -->
         <!-- REQUIRED SCRIPTS -->
         <!-- jQuery -->
-        <script src="<?php base_url() ?>assets/js/jquery/jquery.js"></script>
-        <script src="<?php base_url() ?>assets/js/auth/login.js"></script>
+        <script src="<?= base_url() ?>assets/js/jquery/jquery.js"></script>
+        <script src="<?= base_url() ?>assets/js/auth/login.js"></script>
     </body>
 </html>
