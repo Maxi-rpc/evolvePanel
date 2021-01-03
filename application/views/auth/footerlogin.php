d7a33b429adb7fb7d4664ab6f45c3ad0c762d37f
Maximiliano <maxirpc2607@gmail.com>
2021-01-03 17:23:00 -0300

    Update footerlogin.php

--- application/views/auth/footerlogin.php
+++ application/views/auth/footerlogin.php
@@ -4,6 +4,7 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
+<script src="<?php base_url() ?>assets/js/jquery/jquery.js"></script>
 <script src="<?php base_url() ?>assets/js/auth/login.js"></script>
   </body>
 </html>
