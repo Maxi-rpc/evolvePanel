b1ca215c6b036e66be07a74635108ef4c78f5ed9
Maximiliano <maxirpc2607@gmail.com>
2021-01-03 17:52:37 -0300

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -30,45 +30,41 @@
             <!-- /.login-logo -->
             <div class="card">
                 <div class="card-body login-card-body">
-                <p class="login-box-msg">Iniciar Sesión</p>
-                <?= validation_errors() ?>
-                <form action="<?= base_url('login/validate'); ?>" method="POST" id="formLogin">
-                    <div class="input-group mb-3" id="email">
-                    <input name="email" type="email" class="form-control" placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                        <span class="fas fa-envelope"></span>
+                    <p class="login-box-msg">Iniciar Sesión</p>
+                    <form action="<?= base_url('login/validate'); ?>" method="POST" id="formLogin">
+                        <div class="input-group mb-3" id="email">
+                        <input name="email" type="email" class="form-control" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="invalid-feedback"></div>
-                    </div>
-                    <div class="input-group mb-3" id="password">
-                    <input name="password" type="password" class="form-control" placeholder="Password">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                        <span class="fas fa-lock"></span>
+                        <div class="invalid-feedback"></div>
                         </div>
-                    </div>
-                    <div class="invalid-feedback"></div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
-                    </div>
-                    <!-- /.col -->
-                    </div>
-                </form>
-                <p class="mb-1">
-                    <a href="forgot-password.html">Olvidé mi contraseña</a>
-                </p>
+                        <div class="input-group mb-3" id="password">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        <div class="invalid-feedback"></div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+                        </div>
+                        <!-- /.col -->
+                        </div>
+                    </form>
+                    <p class="mb-1">
+                        <a href="forgot-password.html">Olvidé mi contraseña</a>
+                    </p>
                 </div>
                 <!-- /.login-card-body -->
             </div>
-            </div>
-        <!-- /.login-box -->
         </div>
-        <!-- ./wrapper -->
-
+        <!-- /.login-box -->
         <!-- REQUIRED SCRIPTS -->
         <!-- jQuery -->
         <script src="<?php base_url() ?>assets/js/jquery/jquery.js"></script>
