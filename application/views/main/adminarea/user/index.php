4362526eb4393af3e68ff6f1660211f249d8390d
Maximiliano <maxirpc2607@gmail.com>
2021-01-04 14:19:36 -0300

    Update index.php

--- application/views/main/adminarea/user/index.php
+++ application/views/main/adminarea/user/index.php
@@ -32,7 +32,7 @@
               <div class="col-md-12">
                 <div class="card card-primary">
                   <div class="card-header">
-                    <h3 class="card-title">Listado de usuarios - <span><?php $datosSql['cantUser'] ?></span></h3>
+                    <h3 class="card-title">Listado de usuarios - <span><?=$datosSql['cantUser']?></span></h3>
                   </div>
                   <!-- /.card-header -->
                   <div class="card-body">
