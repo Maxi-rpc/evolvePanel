7306fc0aec7d318d7631e58a1fe59a627780add4
Maximiliano <maxirpc2607@gmail.com>
2021-01-05 14:22:08 -0300

    Update edit.php

--- application/views/main/adminarea/user/edit.php
+++ application/views/main/adminarea/user/edit.php
@@ -72,7 +72,7 @@
                             </div>
                             <!-- /.card-body -->
                             <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
                             </div>
                         </form>
                     </div>
