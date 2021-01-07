592e1a9fc16e79026f587d451db8a8699034d729
Maximiliano <maxirpc2607@gmail.com>
2021-01-07 13:54:57 -0300

    Fix footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -17,22 +17,22 @@
     <!-- REQUIRED SCRIPTS -->
 
     <!-- jQuery -->
-    <script src="<?= base_url() ?>assets/js/jquery/jquery.min.js"></script>
+    <script src="<?=base_url()?>assets/js/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="<?= base_url() ?>assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=base_url()?>assets/js/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="<?= base_url() ?>assets/js/adminlte.min.js"></script>
+    <script src="<?=base_url()?>assets/js/adminlte.min.js"></script>
 
     <!-- DataTables -->
-    <script src="<?= base_url() ?>assets/js/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url() ?>assets/js/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?= base_url() ?>assets/js/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="<?= base_url() ?>assets/js/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="<?=base_url()?>assets/js/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?=base_url()?>assets/js/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?=base_url()?>assets/js/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?=base_url()?>assets/js/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 
     <!-- Toastr -->
-    <script src="<?= base_url() ?>assets/js/toastr/toastr.min.js"></script>
+    <script src="<?=base_url()?>assets/js/toastr/toastr.min.js"></script>
     <!-- SweetAlert2 -->
-    <script src="<?= base_url() ?>assets/js/sweetalert2/sweetalert2.min.js"></script>
+    <script src="<?=base_url()?>assets/js/sweetalert2/sweetalert2.min.js"></script>
 
     <script>
   $(function () {
