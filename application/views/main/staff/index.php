fe4c9771da1309f3dc9f774bdab1c99d0dab25d0
Maximiliano <maxirpc2607@gmail.com>
2021-01-07 14:17:28 -0300

    Update index.php

--- application/views/main/staff/index.php
+++ application/views/main/staff/index.php
@@ -22,8 +22,64 @@
     <div class="content">
       <div class="container-fluid">
       <!-- =========================================================== -->
-        
 
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Listado de todo el Staff</h3>
+                    </div> <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Nickname</th>
+                                    <th>Email</th>
+                                    <th>Sector</th>
+                                    <th>Puesto</th>
+                                    <th>Acción</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if($userInfo != NULL): ?>
+                                    <?php foreach($userInfo as $user): ?>
+                                        <tr>
+                                            <td><?= $user->nombre?></td>
+                                            <td><?= $user->apellido?></td>
+                                            <td><?= $user->nickname?></td>
+                                            <td><?= $user->email?></td>
+                                            <td><?= $user->sector?></td>
+                                            <td><?= $user->puesto?></td>
+                                            <td>
+                                                <button type="button" class="btn btn-tool"><a href="<?=base_url('staff/search/'.$user->id)?>"><i class="fas fa-search-plus"></i></a>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach ?>
+                                    <?php else: ?>
+                                        <tr>
+                                            <td class="text-center">No existen datos para mostrar.</td>
+                                        </tr>
+                                <?php endif ?>
+                            </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Nickname</th>
+                                <th>Email</th>
+                                <th>Sector</th>
+                                <th>Puesto</th>
+                                <th>Acción</th>
+                            </tr>
+                        </tfoot>
+                        </table>
+                    </div> <!-- /.card-body -->
+                </div> <!-- /.card -->
+            </div> <!-- /.col-12 -->
+        </div> <!-- /.row -->
         
       <!-- =========================================================== -->
       </div><!-- /.container-fluid -->
