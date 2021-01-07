4132d0279a045cf5553d157aadfec69086884b74
Maximiliano <maxirpc2607@gmail.com>
2021-01-07 13:36:29 -0300

    staff

---
+++ application/controllers/Staff.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Staff extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+        $this->load->helper('vistas');
+        $this->load->model(['UsersInfo_m','Sector_m','Puesto_m']);
+	}
+
+	public function index()
+	{
+        $data['userInfo'] = $this->UserInfo_m->get();
+		$data['sectores'] = $this->Sector_m->get();
+        $data['puestos'] = $this->Puesto_m->get();
+        
+        $vista = $this->load->view('main/staff/index',$data,TRUE);
+		getTemplate($vista);
+    }
+
+}
