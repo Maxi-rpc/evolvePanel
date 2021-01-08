b250e4b6f1813d81066815fab6f733f2d8308d77
Maximiliano <maxirpc2607@gmail.com>
2021-01-07 23:10:58 -0300

    Update preContent.php

--- application/views/includes/preContent.php
+++ application/views/includes/preContent.php
@@ -5,12 +5,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Ejemplo</h1>
+            <h1 class="m-0 text-dark">Locacion</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Ejemplo</li>
+              <li class="breadcrumb-item active">Locacion</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
