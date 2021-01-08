e63922f30cf8da871afd1a62ecf27e38724865c7
Maximiliano <maxirpc2607@gmail.com>
2021-01-08 14:20:08 -0300

    Create edit.php

---
+++ application/views/main/team/lol/edit.php
@@ -0,0 +1,34 @@
+<!-- =========================================================== -->
+
+            <div class="row">
+              <div class="col-12">
+                <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title"><?php echo empty($partidas->nombre) ? 'Nueva Partida':'Editar Partida';?></h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <div class="card-body">
+                          <?php if(validation_errors()){ ?>
+                            <div class="alert alert-danger alert-dismissible">
+                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h5><i class="icon fas fa-ban"></i> Error!</h5>
+                                <?= validation_errors('<li>','</li>') ?>
+                            </div>
+                          <?php } ?>
+                          <?php echo form_open() ?>
+                            <div class="form-group">
+                              <label for="">Ingresar Nombre</label>
+                              <input type="text" class="form-control" id="exampleInputEmail1" name="nombre" value="<?php echo set_value('name',$partidas->nombre) ?>">
+                            </div>
+                        </div> <!-- /.card-body -->
+                            
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                          <?php echo form_close() ?>
+                </div>
+              </div>
+            </div>
+
+<!-- =========================================================== -->
