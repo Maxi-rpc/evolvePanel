9c2b22afc2c1f7e3b1691f0f15b817a66b1d8572
Maximiliano <maxirpc2607@gmail.com>
2021-01-08 13:53:55 -0300

    Update search.php

--- application/views/main/team/lol/search.php
+++ application/views/main/team/lol/search.php
@@ -2,7 +2,7 @@
 
 <div class="row">
         <div class="col-12">
-            <div class="card card-info">
+            <div class="card card-primary">
               <div class="card-header">
                 <h3 class="card-title">Partida buscada - Num Partida</h3>
 
