9d00f9978f9962728b1537706a98fb2c5f67a6a1
Maximiliano <maxirpc2607@gmail.com>
2021-01-08 11:55:00 -0300

    Update index.php

--- application/views/main/adminarea/team/index.php
+++ application/views/main/adminarea/team/index.php
@@ -5,7 +5,7 @@
                   <div class="col-md-12">
                     <div class="alert alert-success alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                          <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+                          <h5><i class="icon fas fa-exclamation-triangle"></i> Alert!</h5>
                           <?= $this->session->flashdata('msj') ?>
                     </div>
                   </div>
