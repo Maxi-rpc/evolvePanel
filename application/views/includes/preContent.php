8a125b7a1b83f7c219204c80db0cc40d2c3f2d6e
Maximiliano <maxirpc2607@gmail.com>
2021-01-08 00:58:24 -0300

    Update preContent.php

--- application/views/includes/preContent.php
+++ application/views/includes/preContent.php
@@ -10,7 +10,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Locacion</li>
+              <li class="breadcrumb-item active"><?= $loc ?></li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
