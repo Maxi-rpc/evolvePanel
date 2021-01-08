c7d99e5f6c5ba1de11de6ddd431170bf92e27316
Maximiliano <maxirpc2607@gmail.com>
2021-01-08 13:50:29 -0300

    Update search.php

--- application/views/main/team/lol/search.php
+++ application/views/main/team/lol/search.php
@@ -1,5 +1,24 @@
 <!-- =========================================================== -->
 
+<div class="row">
+        <div class="col-md-3">
+            <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">Partida buscada</h3>
 
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="maximize"><i class="fas fa-expand"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div> <!-- /.card-tools -->
+                
+              </div> <!-- /.card-header -->
+              
+              <div class="card-body">
+                listado
+              </div> <!-- /.card-body -->
+            </div> <!-- /.card -->
+        </div> <!-- /.col -->
+</div>
 
 <!-- =========================================================== -->
